5c69ea7b3ea8d827dc7bbb8c5efdeff6f2669e4a
owalrohit <owalrohit03@gmail.com>
2022-09-26 15:30:32 +0530

    finally done

--- rohit/src/test/TestDemo.java
+++ rohit/src/test/TestDemo.java
@@ -3,9 +3,9 @@ package test;
 public class TestDemo {
 
 	public static void main(String[] args) {
-		System.out.println("hello user");
+		System.out.println("hello Rohit");
 		System.out.println("welcome to git");
-		System.out.println("welcome to git");
+		
 		
 	}
 }
